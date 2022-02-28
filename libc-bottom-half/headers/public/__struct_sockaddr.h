ff3460b3cff074528b55fa8608415d42093711ba
Kim Stonehouse <kimbethstonehouse@gmail.com>
2022-02-28 21:45:00 +0000

    Update __struct_sockaddr.h

--- libc-bottom-half/headers/public/__struct_sockaddr.h
+++ libc-bottom-half/headers/public/__struct_sockaddr.h
@@ -7,7 +7,7 @@
 #include <__typedef_sa_family_t.h>
 
 struct sockaddr {
-    _Alignas(max_align_t) sa_family_t sa_family;
+    _Alignas(unsigned short int) sa_family_t sa_family;
     char sa_data[0];
 };
 
